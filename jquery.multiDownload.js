8c5d5835d95bd9baa748ee4085112e81dc1fda4e
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-06-25 23:17:22 +0200

    Removed links private variable

--- jquery.multiDownload.js
+++ jquery.multiDownload.js
@@ -1,39 +1,29 @@
 (function( $ ){
 
-    var links = [],
-    methods = {
-        add: function() { 
+    var methods = {
+        add: function () {
             return this.each(function() {
                 $(this).addClass('multi-download-item');
-                links.push(this);
             });
         },
 
         remove: function () {
-            return this.each(function () {
-                $(this).removeClass('multi-download-item');
-                links.pop(this);
-            });
-        },
-
-        clear: function () {
-            $.each(links, function (index, link) {
-                $(link).removeClass('multi-download-item');
-            });
-            links = [];
-            return this;
+            var links = this.length ? this : $('.multi-download-item');
+            links.removeClass('multi-download-item');
+            return $('.multi-download-item');
         },
 
         bind: function( e, options ) {
             var delay = (options && options.delay) || 1000;
+            var links = $('.multi-download-item');
             return this.each(function () {
                 $(this).addClass('multi-download-trigger');
-                $(this).bind(e, function(event) {
+                $(this).bind(e, function (event) {
                     event.preventDefault();
-                    $.each(links, function (index, element) {
+                    $(links).each(function () {
                         var frame = $('<iframe style="display: none;" class="multi-download-frame"></iframe>');
-                        frame.attr('src', $(element).attr('href'));
-                        $(element).after(frame);
+                        frame.attr('src', $(this).attr('href'));
+                        $(this).after(frame);
                         setTimeout(function () { frame.remove(); }, delay);
                     });
                 });
@@ -56,10 +46,6 @@
 
     $.fn.multiDownloadRemove = function () {
         return methods.remove.apply( this, arguments );
-    };
-
-    $.fn.multiDownloadClear = function () {
-        return methods.clear.apply( this, arguments );
     };
 
 })( jQuery );
