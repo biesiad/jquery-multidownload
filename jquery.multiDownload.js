60a775d089e6dfeeadcaf31b75f5533ef6de0373
Allison <alamm@globalscape.com>
2014-10-31 16:58:33 -0500

    Event listener added properly

--- jquery.multiDownload.js
+++ jquery.multiDownload.js
@@ -4,19 +4,19 @@
         _download: function (options) {
             var triggerDelay = (options && options.delay) || 100;
             var cleaningDelay = (options && options.cleaningDelay) || 1000;
-            var onloadEvent = (options && options.onloadEvent) || "";
+            var onLoadHandler = (options && options.onLoadHandler);
 
             this.each(function (index, item) {
-                methods._createIFrame(item, index * triggerDelay, cleaningDelay, onloadEvent);
+                methods._createIFrame(item, index * triggerDelay, cleaningDelay, onLoadHandler);
             });
             return this;
         },
 
-        _createIFrame: function (item, triggerDelay, cleaningDelay, onloadEvent) {
+        _createIFrame: function (item, triggerDelay, cleaningDelay, onLoadHandler) {
             setTimeout(function () {
                 var frame = $('<iframe style="display: none;" class="multi-download-frame"></iframe>');
-                if(onloadEvent != "") {
-                    frame = $('<iframe style="display: none;" onload="' + onloadEvent + '(this);"  class="multi-download-frame"></iframe>');
+                if(onLoadHandler) {
+                    frame.on('load', onLoadHandler)
                 }
                 frame.attr('src', $(item).attr('href') || $(item).attr('src'));
                 $(item).after(frame);
