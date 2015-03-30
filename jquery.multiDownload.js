e235333c36e54bc7674cce5edbb0aebed161fbea
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2015-03-29 22:01:12 -0700

    Cleaning up the code

--- jquery.multiDownload.js
+++ jquery.multiDownload.js
@@ -1,32 +1,34 @@
-(function ($) {
+;(function($, window, document, undefined) {
+  "use strict"
 
-    var methods = {
-        _download: function (options) {
-            var triggerDelay = (options && options.delay) || 100;
-            var cleaningDelay = (options && options.cleaningDelay) || 1000;
-            var onLoadHandler = (options && options.onLoadHandler);
+  var download = function (options) {
+    var triggerDelay = (options && options.delay) || 100
+    var cleaningDelay = (options && options.cleaningDelay) || 1000
+    var onLoadHandler = (options && options.onLoadHandler)
 
-            this.each(function (index, item) {
-                methods._createIFrame(item, index * triggerDelay, cleaningDelay, onLoadHandler);
-            });
-            return this;
-        },
+    this.each(function (index, item) {
+      createIFrame(item, index * triggerDelay, cleaningDelay, onLoadHandler)
+    })
+    return this
+  }
 
-        _createIFrame: function (item, triggerDelay, cleaningDelay, onLoadHandler) {
-            setTimeout(function () {
-                var frame = $('<iframe style="display: none;" class="multi-download-frame"></iframe>');
-                if(onLoadHandler) {
-                    frame.on('load', onLoadHandler)
-                }
-                frame.attr('src', $(item).attr('href') || $(item).attr('src'));
-                $(item).after(frame);
-                setTimeout(function () { frame.remove(); }, cleaningDelay);
-            }, triggerDelay);
-        }
-    };
+  var createIFrame = function (item, triggerDelay, cleaningDelay, onLoadHandler) {
+    setTimeout(function () {
+      var frame = $('<iframe style="display: none;" class="multi-download-frame"></iframe>')
 
-    $.fn.multiDownload = function(options) {
-        return methods._download.apply(this, arguments);
-    };
+      if(onLoadHandler) {
+        frame.on('load', onLoadHandler)
+      }
 
-})(jQuery);
+      frame.attr('src', $(item).attr('href') || $(item).attr('src'))
+      $(item).after(frame)
+
+      setTimeout(function () { frame.remove() }, cleaningDelay)
+    }, triggerDelay)
+  }
+
+  $.fn.multiDownload = function(options) {
+      return download.call(this, options)
+  }
+
+})(jQuery, window, document);
